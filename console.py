7c1982bd97155e033e2e6caf8cc7267f20c629e0
Esraa413 <esraaabdelwahab614@gmail.com>
2023-12-10 15:34:31 -0800

    console

--- console.py
+++ console.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines the HBnB console."""
+"""Function BnB console."""
 import cmd
 import re
 from shlex import split
@@ -32,10 +32,10 @@ def parse(arg):
 
 
 class HBNBCommand(cmd.Cmd):
-    """Defines the HolbertonBnB command interpreter.
+    """Function HolbertonBnB command interpreter.
 
     Attributes:
-        prompt (str): The command prompt.
+        prompt (str): command prompt.
     """
 
     prompt = "(hbnb) "
@@ -50,11 +50,11 @@ class HBNBCommand(cmd.Cmd):
     }
 
     def emptyline(self):
-        """Do nothing upon receiving an empty line."""
+        """Do nothing upon receiv an emptythe lien."""
         pass
 
     def default(self, arg):
-        """Default behavior for cmd module when input is invalid"""
+        """Function cmd module when input is invalid"""
         argdict = {
             "all": self.do_all,
             "show": self.do_show,
@@ -75,17 +75,17 @@ class HBNBCommand(cmd.Cmd):
         return False
 
     def do_quit(self, arg):
-        """Quit command to exit the program."""
+        """Quit command  program."""
         return True
 
     def do_EOF(self, arg):
-        """EOF signal to exit the program."""
+        """EOF signal to exit."""
         print("")
         return True
 
     def do_create(self, arg):
-        """Usage: create <class>
-        Create a new class instance and print its id.
+        """Usage: created<class>
+        Create a new class and print its id.
         """
         argl = parse(arg)
         if len(argl) == 0:
@@ -97,8 +97,8 @@ class HBNBCommand(cmd.Cmd):
             storage.save()
 
     def do_show(self, arg):
-        """Usage: show <class> <id> or <class>.show(<id>)
-        Display the string representation of a class instance of a given id.
+        """Usage: show <class> <id> and <class>.show(<id>)
+        Display representation of a class instance of a  id.
         """
         argl = parse(arg)
         objdict = storage.all()
@@ -114,8 +114,8 @@ class HBNBCommand(cmd.Cmd):
             print(objdict["{}.{}".format(argl[0], argl[1])])
 
     def do_destroy(self, arg):
-        """Usage: destroy <class> <id> or <class>.destroy(<id>)
-        Delete a class instance of a given id."""
+        """Usage: destroy <class> <id> and <class>.destroy(<id>)
+        Delete a class instance of a  id."""
         argl = parse(arg)
         objdict = storage.all()
         if len(argl) == 0:
@@ -131,9 +131,9 @@ class HBNBCommand(cmd.Cmd):
             storage.save()
 
     def do_all(self, arg):
-        """Usage: all or all <class> or <class>.all()
-        Display string representations of all instances of a given class.
-        If no class is specified, displays all instantiated objects."""
+        """Usage: all or all <class> and <class>.all()
+        Display representations of all instances of a  class.
+        If no class specified, displays instantiated objects."""
         argl = parse(arg)
         if len(argl) > 0 and argl[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
@@ -147,8 +147,8 @@ class HBNBCommand(cmd.Cmd):
             print(objl)
 
     def do_count(self, arg):
-        """Usage: count <class> or <class>.count()
-        Retrieve the number of instances of a given class."""
+        """Usage: count <class> and <class>.count()
+        Retrieve the number of the instances of a class."""
         argl = parse(arg)
         count = 0
         for obj in storage.all().values():
@@ -160,8 +160,8 @@ class HBNBCommand(cmd.Cmd):
         """Usage: update <class> <id> <attribute_name> <attribute_value> or
        <class>.update(<id>, <attribute_name>, <attribute_value>) or
        <class>.update(<id>, <dictionary>)
-        Update a class instance of a given id by adding or updating
-        a given attribute key/value pair or dictionary."""
+        Update class instance of a id by adding or updating
+        a given attribute key/value ."""
         argl = parse(arg)
         objdict = storage.all()
 
