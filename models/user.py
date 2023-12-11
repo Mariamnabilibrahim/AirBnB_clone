0913fbe40c8267f4d6904d8dc2d2091a7da4f539
Mariam <eng.mariamnabil93@gmail.com>
2023-12-10 18:41:13 -0800

    1

--- models/user.py
+++ models/user.py
@@ -7,10 +7,10 @@ class User(BaseModel):
     """Represent a User.
 
     Attributes:
-        email (str): The email of the user.
-        password (str): The password of the user.
-        first_name (str): The first name of the user.
-        last_name (str): The last name of the user.
+        email (str): The email of user.
+        password (str): The password of user.
+        first_name (str): The first name of user.
+        last_name (str): The last name of user.
     """
 
     email = ""
